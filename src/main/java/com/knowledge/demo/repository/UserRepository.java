456b55b551b2c3330496351a09dbc96983075764
SrLucasGaldinor <lucasgaldinorosa2904@gmail.com>
2026-03-13 18:37:16 -0300

    Adicionar buscas personalizadas por ID, Email e CPF

--- src/main/java/com/knowledge/demo/repository/UserRepository.java
+++ src/main/java/com/knowledge/demo/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.knowledge.demo.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,5 +12,7 @@ import com.knowledge.demo.entity.User;
 public interface UserRepository extends JpaRepository<User, Long>{
 
 	List<User> findAllByActiveTrue();
-	
+	Optional<User> findByIdAndActiveTrue(Long id);
+	Optional<User> findByEmailAndActiveTrue(String email);
+	Optional<User> findByCpfAndActiveTrue(String cpf);
 }
