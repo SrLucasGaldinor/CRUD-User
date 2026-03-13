2764a7b8899bfb841af4f4038bf6778adce2deed
SrLucasGaldinor <lucasgaldinorosa2904@gmail.com>
2026-03-13 18:24:27 -0300

    Classe de UserResponseDTO

--- src/main/java/com/knowledge/demo/DTO/UserResponseDTO.java
+++ src/main/java/com/knowledge/demo/DTO/UserResponseDTO.java
@@ -1,14 +1,5 @@
 package com.knowledge.demo.DTO;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
-public class UserResponseDTO {
-
-	private String name;
-	private String email;
-	private String phone;
-	private String cpf;
+public record UserResponseDTO (String username, String email, String phone, String cpf) {
+	
 }
