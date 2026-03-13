b03f793087670721ca373d539de5d960fe27530d
SrLucasGaldinor <lucasgaldinorosa2904@gmail.com>
2026-03-13 18:38:04 -0300

    Adicionar métodos GET e POST

--- src/main/java/com/knowledge/demo/controller/UserController.java
+++ src/main/java/com/knowledge/demo/controller/UserController.java
@@ -1,10 +1,36 @@
 package com.knowledge.demo.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.knowledge.demo.DTO.UserCreateDTO;
+import com.knowledge.demo.DTO.UserResponseDTO;
+import com.knowledge.demo.service.UserService;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
+	private final UserService service;
+
+	public UserController(UserService service) {
+		this.service = service;
+	}
+
+	@GetMapping
+	public ResponseEntity<List<UserResponseDTO>> findAll() {
+		return ResponseEntity.ok(service.findAll());
+	}
+
+	@PostMapping
+	public ResponseEntity<UserResponseDTO> create(@RequestBody UserCreateDTO dto) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(service.create(dto));
+	}
 }
