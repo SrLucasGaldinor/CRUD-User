126ac459d705cd45adefdc5306e0fec2a6b12f62
SrLucasGaldinor <lucasgaldinorosa2904@gmail.com>
2026-03-17 15:01:52 -0300

    'Seed' para testes no H2 usando CommandLineRunner

---
+++ src/main/java/com/knowledge/demo/SetupConfig.java
@@ -0,0 +1,41 @@
+package com.knowledge.demo;
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.knowledge.demo.entity.User;
+import com.knowledge.demo.repository.UserRepository;
+
+@Configuration
+@Profile("test")
+public class SetupConfig {
+
+
+	private final UserRepository repository;
+
+	public SetupConfig(UserRepository repository) {
+		this.repository = repository;
+	}
+
+    @Bean
+    CommandLineRunner initDatabase() {
+		return args -> {
+			User user1 = User.builder().username("Erick").email("erick@email.com").password("123456").phone("9999-9999")
+					.cpf("111.111.111-11").build();
+
+			User user2 = User.builder().username("Lucas").email("lucas@email.com").password("123456").phone("9999-9999")
+					.cpf("222.222.222-22").build();
+
+			User user3 = User.builder().username("Gustavo").email("gustavo@email.com").password("123456")
+					.phone("9999-9999").cpf("333.333.333-33").build();
+
+			repository.saveAll(Arrays.asList(user1, user2, user3));
+			
+			System.out.println("Dados inicializados com sucesso!");
+		};
+	}
+}
