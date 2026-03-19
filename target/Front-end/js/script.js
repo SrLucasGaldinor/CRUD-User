8c668f32aa4f213e80b2a42be4a408e35b987cb1
zendron-erick <zendron.erick@gmail.com>
2026-03-19 19:25:54 -0300

    Finalizando interface: botões grandes, feedback no canto e correções JS

--- target/Front-end/js/script.js
+++ target/Front-end/js/script.js
@@ -8,26 +8,29 @@ form.addEventListener('submit', function(event) {
 
     const email = document.getElementById('email').value;
     const confirmEmail = document.getElementById('confirm-email').value
-    const password = document.getElementById('password').Value;
+    const password = document.getElementById('password').value;
     const confirmPassword = document.getElementById('confirm-password').value;
 
-    if (email != confirmEmail){
+    console.log("Valor da Senha 1:", password);
+    console.log("Valor da Senha 2:", confirmPassword);
+
+    if (email !== confirmEmail){
         msg.style.display = 'block';
         msg.style.backgroundColor = "#ff4d4d";
         msg.innerHTML = "<strong>Error:</strong> The emails don't match! Please  try again.";
         return
     }
-    if (password != confirmPassword){
+    if (password !== confirmPassword){
         msg.style.display = 'block';
         msg.style.backgroundColor = "#ff4d4d";
         msg.innerHTML = "<strong>Error:</strong> The passwords don't match! Please try again.";
         return
     }
 
-    msgFeedback.style.display = 'block';
-    msg.style.backgroundColor = "#000000"
-    msg.style.color = "#000";
-    msgFeedback.innerHTML = "Registration successful! Redirecting...";
+    msg.style.display = 'block';
+    msg.style.backgroundColor = "#2ecc71"
+    msg.style.color = "#ffffff";
+    msg.innerHTML = "Registration successful! Redirecting...";
 
     setTimeout(function() {
         window.location.href = "index.html";
